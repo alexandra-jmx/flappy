832d8527a2038eff7d37a8956038b5edd7f5f2e7
alexandra-jmx <alexandra.jmx@gmail.com>
2023-06-24 17:22:23 -0300

    Criação dos principais arquivos e próximos (pequenos) passos

---
+++ js/cena-jogo.js
@@ -0,0 +1,28 @@
+// TODO
+// - importar o chão e o jogador
+// - Obstáculos (? -> Pipes, comida saudável, 'junk food' )
+
+export default class CenaJogo extends Phaser.Scene {
+    constructor() {
+        super({
+            key: 'CenaJogo'
+        });
+    }
+
+    // TODO
+    // - Obstáculos
+    // - Score
+   
+
+    preload() {
+
+    }
+
+    create() {
+
+    }
+
+    update() {
+
+    }
+}
